2ca9ec51c8ad8a5dd533b3075d91f86a2be0d393
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2023-08-17 13:45:11 +0300

    setup for vercel deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+const URL = process.env.MONGO_URL;
+
+if (!URL) {
+  throw new Error('URL is not found');
+}
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(URL)],
   controllers: [AppController],
   providers: [],
 })
